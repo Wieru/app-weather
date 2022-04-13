fafbfbbdfa7f16ebe425bf15bb58f017d9764c11
Szymon Wierucki <szymon.wierucki@gmail.com>
2022-04-13 19:44:02 +0200

    Change files

--- src/components/Result.js
+++ src/components/Result.js
@@ -11,10 +11,11 @@ if(!err && city) {
     const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString()
     const sunsetTime = new Date(sunset * 1000).toLocaleTimeString()
     const tempFormat = Math.round(temp);
+    const cityName = city.charAt(0).toUpperCase() + city.slice(1);
 
     content = (
         <React.Fragment>
-            <h3><em>{city}</em></h3>
+            <h3><em>{cityName}</em></h3>
             <h4>Dzień i godzina: {date}</h4>
             <h4>Temperatura: {tempFormat} &#176;C</h4>
             <h4>Wschód słońca: {sunriseTime}</h4>
