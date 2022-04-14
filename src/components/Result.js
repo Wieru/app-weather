f0fc340b1970f33f4ce8f73bc0dfa090e515c23c
Szymon Wierucki <szymon.wierucki@gmail.com>
2022-04-14 17:19:53 +0200

    Change styles

--- src/components/Result.js
+++ src/components/Result.js
@@ -17,7 +17,7 @@ if(!err && city) {
         <React.Fragment>
             <h3><em>{cityName}</em></h3>
             <h4>Dzień i godzina: {date}</h4>
-            <h4>Temperatura: {tempFormat} &#176;C</h4>
+            <h4>Temperatura: {tempFormat}&#176;C</h4>
             <h4>Wschód słońca: {sunriseTime}</h4>
             <h4>Zachód słońca: {sunsetTime}</h4>
             <h4>Siła wiatru: {wind} m/s</h4>
